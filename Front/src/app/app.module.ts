712eeb0340c8f4482c8500eb154d44faafcb6495
Alfonso Leon Macedo <alfonso.leon@elektra.com.mx>
2025-08-20 16:42:33 -0600

    chore: modificar puerto del socket en frontend para que apunte al correcto

--- Front/src/app/app.module.ts
+++ Front/src/app/app.module.ts
@@ -12,7 +12,7 @@ import { SensorHumedadComponent } from './sensor-humedad/sensor-humedad.componen
 import { FormsModule } from '@angular/forms';
 import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
 
-const config: SocketIoConfig = { url: 'http://localhost:3000', options: {} };
+const config: SocketIoConfig = { url: 'http://localhost:4000', options: {} };
 
 @NgModule({
   declarations: [
